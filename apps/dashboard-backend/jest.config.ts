1193528d445b0e61246ee5a72a45d0206306233f
nicolaslabbe <nicolas.labbe87@gmail.com>
2025-08-20 21:16:06 +0200

    rename

--- apps/dashboard-backend/jest.config.ts
+++ apps/dashboard-backend/jest.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 import { readFileSync } from 'fs';
 
 // Reading the SWC compilation config for the spec files
@@ -10,15 +10,12 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@dashboard/dashboard-e2e',
+  displayName: '@dashboard/dashboard-backend',
   preset: '../../jest.preset.js',
-  globalSetup: '<rootDir>/src/support/global-setup.ts',
-  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
-  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage',
+  coverageDirectory: 'test-output/jest/coverage'
 };
